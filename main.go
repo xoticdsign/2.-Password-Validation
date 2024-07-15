bf7acca7d86e55173e197ce906b067f58ae17445
xoti$ <xoticdollarsign@outlook.com>
2024-07-16 02:11:34 +0300

    A lot of work to be done...

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	h := http.FileServer(http.Dir("./static"))
+	http.Handle("/static", http.StripPrefix("/static", h))
+
+	http.HandleFunc("/", MainPageHandler)
+	http.HandleFunc("/authorization", AuthorizationHandler)
+	http.HandleFunc("/profile", ProfileHandler)
+
+	fmt.Println("Server -> Server started at :8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Server -> Server failed to start!")
+	}
+}
+
+func MainPageHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func AuthorizationHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "authorization.html")
+}
+
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "profile.html")
 }
