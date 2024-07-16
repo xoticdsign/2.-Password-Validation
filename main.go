7eba14c32283d0e765466b68f956f7e62a0c04dc
xoti$ <xoticdollarsign@outlook.com>
2024-07-16 03:05:14 +0300

    Mid process

--- main.go
+++ main.go
@@ -6,29 +6,13 @@ import (
 )
 
 func main() {
-	h := http.FileServer(http.Dir("./static"))
-	http.Handle("/static", http.StripPrefix("/static", h))
+	h := http.FileServer(http.Dir("static/"))
+	http.Handle("/", http.StripPrefix("/", h))
 
-	http.HandleFunc("/", MainPageHandler)
-	http.HandleFunc("/authorization", AuthorizationHandler)
-	http.HandleFunc("/profile", ProfileHandler)
+	fmt.Println("Server -> Server started at :8090")
 
-	fmt.Println("Server -> Server started at :8080")
-
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		fmt.Println("Server -> Server failed to start!")
 	}
-}
-
-func MainPageHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
-}
-
-func AuthorizationHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "authorization.html")
-}
-
-func ProfileHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "profile.html")
 }
