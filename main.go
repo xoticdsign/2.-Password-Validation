924bd5e58510e8c72e3e3bba651b6fb254d3a33c
xoti$ <xoticdollarsign@outlook.com>
2024-07-18 00:26:05 +0300

    Final

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
+var users = map[string]string{"user": "admin", "admin": "123"}
+
 func main() {
-	h := http.FileServer(http.Dir("static/"))
-	http.Handle("/", http.StripPrefix("/", h))
 
-	fmt.Println("Server -> Server started at :8090")
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/", http.StripPrefix("/", fs))
 
-	err := http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/authorization", AuthorizationHandler)
+	http.HandleFunc("/login", LoginHandler)
+	http.HandleFunc("/profile", ProfileHandler)
+
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println("Server -> Server failed to start!")
+		log.Fatal()
 	}
+}
+
+func AuthorizationHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/authorization.html")
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Wrong method", http.StatusMethodNotAllowed)
+	}
+	usernamePOST := r.FormValue("username")
+	passwordPOST := r.FormValue("password")
+
+	for username, password := range users {
+		if username == usernamePOST && password == passwordPOST {
+			http.Redirect(w, r, "/profile", http.StatusSeeOther)
+		} else {
+			http.Redirect(w, r, "/authorization", http.StatusSeeOther)
+		}
+	}
+}
+
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/profile.html")
 }
